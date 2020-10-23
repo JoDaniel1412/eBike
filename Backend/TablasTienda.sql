c9ba86841854487dad5fdc004642a517ac332052
Racso08 <oscar080899@gmail.com>
2020-10-22 21:35:56 -0600

    [ADD] Progress in Backend

---
+++ Backend/TablasTienda.sql
@@ -0,0 +1,104 @@
+-- select data base
+USE database_central;
+GO
+
+-- create schemas
+CREATE SCHEMA produccion;
+GO
+
+CREATE SCHEMA ventas;
+GO
+
+-- create tables
+CREATE TABLE produccion.categorias (
+	idCategoria INTEGER PRIMARY KEY,
+	descripcion VARCHAR (255) NOT NULL 
+);
+
+CREATE TABLE produccion.marcas (
+	idMarca INT PRIMARY KEY,
+	nomMarca VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE produccion.productos (
+	idProducto INT PRIMARY KEY,
+	nomProducto VARCHAR (255) NOT NULL,
+	idMarca INT NOT NULL,
+	idCategoria INT NOT NULL,
+	annoModelo SMALLINT NOT NULL,
+	precioVenta DECIMAL (10, 2) NOT NULL,
+	FOREIGN KEY (idCategoria) REFERENCES produccion.categorias (idCategoria) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (idMarca) REFERENCES produccion.marcas (idMarca) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE ventas.clientes (
+	idCliente INT IDENTITY(1,1) PRIMARY KEY,
+	nombre VARCHAR (255) NOT NULL,
+	apellido VARCHAR (255) NOT NULL,
+	telefono VARCHAR (25),
+	email VARCHAR (255) NOT NULL,
+	calle VARCHAR (255),
+	ciudad VARCHAR (50),
+	estado VARCHAR (25),
+	codPostal VARCHAR (5)
+);
+
+CREATE TABLE ventas.tiendas (
+	idTienda INT IDENTITY(1,1) PRIMARY KEY,
+	nomTienda VARCHAR (255) NOT NULL,
+	telefono VARCHAR (25),
+	email VARCHAR (255),
+	calle VARCHAR (255),
+	ciudad VARCHAR (255),
+	estado VARCHAR (10),
+	codPostal VARCHAR (5)
+);
+
+CREATE TABLE ventas.empleados (
+	idEmpleado INT PRIMARY KEY,
+	nombre VARCHAR (50) NOT NULL,
+	apellido VARCHAR (50) NOT NULL,
+	email VARCHAR (255) NOT NULL UNIQUE,
+	telefono VARCHAR (25),
+	activo tinyint NOT NULL,
+	idTienda INT NOT NULL,
+	idJefe INT,
+	FOREIGN KEY (idTienda) REFERENCES ventas.tiendas (idTienda) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (idJefe) REFERENCES ventas.empleados (idEmpleado) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+CREATE TABLE ventas.ordenes (
+	idOrden INT PRIMARY KEY,
+	idCliente INT,
+	estadoOrden tinyint NOT NULL,
+	-- Order status: 1 = Pending; 2 = Processing; 3 = Rejected; 4 = Completed
+	fechaOrden DATE NOT NULL,
+	required_date DATE NOT NULL,
+	fechaEnvio DATE,
+	idTienda INT NOT NULL,
+	idEmpleado INT NOT NULL,
+	FOREIGN KEY (idCliente) REFERENCES ventas.clientes (idCliente) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (idTienda) REFERENCES ventas.tiendas (idTienda) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (idEmpleado) REFERENCES ventas.empleados (idEmpleado) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+CREATE TABLE ventas.detalleOrden (
+	idOrden INT,
+	idItem INT,
+	idProducto INT NOT NULL,
+	cantidad INT NOT NULL,
+	precioVenta DECIMAL (10, 2) NOT NULL,
+	descuento DECIMAL (4, 2) NOT NULL DEFAULT 0,
+	PRIMARY KEY (idOrden, idItem) ,
+	FOREIGN KEY (idOrden) REFERENCES ventas.ordenes (idOrden) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (idProducto) REFERENCES produccion.productos (idProducto) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE produccion.inventario (
+	idTienda INT,
+	idProducto INT,
+	cantidad INT,
+	PRIMARY KEY (idTienda, idProducto),
+	FOREIGN KEY (idTienda) REFERENCES ventas.tiendas (idTienda) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (idProducto) REFERENCES produccion.productos (idProducto) ON DELETE CASCADE ON UPDATE CASCADE
+);
---
+++ Backend/api.py
@@ -0,0 +1,210 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import inspect
+from flask_cors import CORS, cross_origin
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mssql+pyodbc://sa:1234@DESKTOP-A1FP3OB\MSSQLSERVER01/database_central?driver=SQL Server"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class marcas(db.Model):
+    __tablename__ = 'marcas'
+    __table_args__ = {"schema": "produccion"}
+    idMarca = db.Column(db.Integer, primary_key=True)
+    nomMarca = db.Column(db.String(255), nullable = False)
+
+class categorias(db.Model):
+    __tablename__ = 'categorias'
+    __table_args__ = {"schema": "produccion"}
+    idCategoria = db.Column(db.Integer, primary_key=True)
+    descripcion = db.Column(db.String(255), nullable = False)
+
+class tiendas(db.Model):
+    __tablename__ = 'tiendas'
+    __table_args__ = {"schema": "ventas"}
+    idTienda = db.Column(db.Integer, primary_key=True)
+    nomTienda = db.Column(db.String(255), nullable = False)
+    telefono = db.Column(db.String(255), nullable = True)
+    email = db.Column(db.String(255), nullable = True)
+    calle = db.Column(db.String(255), nullable = True)
+    ciudad = db.Column(db.String(255), nullable = True)
+    estado = db.Column(db.String(10), nullable = True)
+    codPostal = db.Column(db.String(5), nullable = True)
+
+class clientes(db.Model):
+    __tablename__ = 'clientes'
+    __table_args__ = {"schema": "ventas"}
+    idCliente = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nombre = db.Column(db.String(255), nullable = False)
+    apellido = db.Column(db.String(255), nullable = False)
+    telefono = db.Column(db.String(255), nullable = True)
+    email = db.Column(db.String(255), nullable = False)
+    calle = db.Column(db.String(255), nullable = True)
+    ciudad = db.Column(db.String(50), nullable = True)
+    estado = db.Column(db.String(25), nullable = True)
+    codPostal = db.Column(db.String(5), nullable = True)
+
+class empleados(db.Model):
+    __tablename__ = 'empleados'
+    __table_args__ = {"schema": "ventas"}
+    idEmpleado = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable = False)
+    apellido = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(255), nullable = False, unique = True)
+    telefono = db.Column(db.String(25), nullable = True)
+    activo = db.Column(db.Integer, nullable = False)
+    idTienda = db.Column(db.Integer, db.ForeignKey('tiendas.idTienda'), nullable = False)
+    idJefe = db.Column(db.Integer, db.ForeignKey('empleados.idEmpleado'), nullable = True)
+
+class ordenes(db.Model):
+    __tablename__ = 'ordenes'
+    __table_args__ = {"schema": "ventas"}
+    idOrden = db.Column(db.Integer, primary_key=True)
+    idCliente = db.Column(db.Integer, db.ForeignKey('clientes.idCliente'), nullable = False)
+    estadoOrden = db.Column(db.Integer, nullable = False)
+    fechaOrden = db.Column(db.Date, nullable = False)
+    required_date = db.Column(db.Date, nullable = False)
+    fechaEnvio = db.Column(db.Date, nullable = True)
+    idTienda = db.Column(db.Integer, db.ForeignKey('tiendas.idTienda'), nullable = False)
+    idEmpleado = db.Column(db.Integer, db.ForeignKey('empleados.idEmpleado'), nullable = False)
+
+
+#----------------------------- Admin View ----------------------------
+
+# Testeo
+@app.route('/marca', methods=['GET'])
+def get_marca():
+
+    all_marcas = marcas.query.with_entities(
+        marcas.idMarca,
+        marcas.nomMarca
+    ).all()
+    
+    result = []
+
+    for marca in all_marcas:
+        new_marca = []
+
+        for data in marca:
+            new_marca.append(data)
+
+        result.append(new_marca)
+
+    return jsonify(result), 200
+
+# Categories
+@app.route('/categories', methods=['GET'])
+def get_category():
+
+    categories = categorias.query.with_entities(
+        categorias.idCategoria,
+        categorias.descripcion
+    ).all()
+    
+    result = []
+
+    for category in categories:
+        new_category = []
+
+        for data in category:
+            new_category.append(data)
+
+        result.append(new_category)
+
+    return jsonify(result), 200
+
+# Store names
+@app.route('/stores', methods=['GET'])
+def get_stores():
+
+    stores = tiendas.query.with_entities(
+        tiendas.idTienda,
+        tiendas.nomTienda,
+        tiendas.telefono,
+        tiendas.email,
+        tiendas.calle,
+        tiendas.ciudad,
+        tiendas.estado,
+        tiendas.codPostal
+    ).all()
+    
+    result = []
+
+    for store in stores:
+        new_store = []
+
+        for data in store:
+            new_store.append(data)
+
+        result.append(new_store)
+
+    return jsonify(result), 200
+
+#Top clients
+@app.route('/clients', methods=['GET'])
+def get_clients():
+
+    clients = clientes.query.with_entities(
+        clientes.idCliente,
+        clientes.nombre,
+        clientes.apellido,
+        clientes.telefono,
+        clientes.email,
+        clientes.calle,
+        clientes.ciudad,
+        clientes.estado,
+        clientes.codPostal
+    ).all()
+    
+    result = []
+
+    for client in clients:
+        new_client = []
+
+        for data in client:
+            new_client.append(data)
+
+        result.append(new_client)
+
+    return jsonify(result), 200
+
+
+# Number of orders per client
+@app.route('/orders/client', methods=['GET'])
+def get_order_client():
+
+    orders = ordenes.query.join(clientes).join(tiendas).join(empleados).with_entities(
+        clientes.idCliente,
+        clientes.nombre,
+        clientes.apellido,
+        clientes.telefono,
+        clientes.email,
+        clientes.calle,
+        clientes.ciudad,
+        clientes.estado,
+        clientes.codPostal,
+        ordenes.estadoOrden
+    ).all()
+    
+    result = []
+
+    for client in clients:
+        new_client = []
+
+        for data in client:
+            new_client.append(data)
+
+        result.append(new_client)
+
+    return jsonify(result), 200
+
+#----------------------------- Run  ----------------------------
+
+if __name__ == "__main__":
+    app.run(debug=True)
